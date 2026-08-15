ed2a7045efc38ac7289427dfc3bb3c45f74f20f3
jhrhq <jhr.code@proton.me>
2026-08-15 23:41:33 +0600

    chore: updated package versions

--- index.js
+++ index.js
@@ -3,84 +3,84 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const ObjectId = require("mongodb").ObjectId;
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const ObjectId = require("mongodb").ObjectId;
 
 //user middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
 
-async function run() {
-  try {
-    await client.connect();
-    const userCollection = client.db("foodExpress").collection("user");
+// async function run() {
+//   try {
+//     await client.connect();
+//     const userCollection = client.db("foodExpress").collection("user");
 
-    // get users as api in the front end
-    app.get("/user", async (req, res) => {
-      const query = {};
+//     // get users as api in the front end
+//     app.get("/user", async (req, res) => {
+//       const query = {};
 
-      const cursor = userCollection.find(query);
-      const users = await cursor.toArray();
-      res.send(users);
-    });
+//       const cursor = userCollection.find(query);
+//       const users = await cursor.toArray();
+//       res.send(users);
+//     });
 
-    // get method: update a user
+//     // get method: update a user
 
-    app.get("/user/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await userCollection.findOne(query);
-      res.send(result);
-    });
+//     app.get("/user/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const query = { _id: ObjectId(id) };
+//       const result = await userCollection.findOne(query);
+//       res.send(result);
+//     });
 
-    // POST User : add a new user in the server
-    app.post("/user", async (req, res) => {
-      const newUser = req.body;
-      console.log("adding newuser", newUser);
-      const result = await userCollection.insertOne(newUser);
-      res.send(result);
-    });
+//     // POST User : add a new user in the server
+//     app.post("/user", async (req, res) => {
+//       const newUser = req.body;
+//       console.log("adding newuser", newUser);
+//       const result = await userCollection.insertOne(newUser);
+//       res.send(result);
+//     });
 
-    // PUT User: update a existing users data
-    app.put("/user/:id", async (req, res) => {
-      const id = req.params.id;
-      const updateUser = req.body;
-      const filter = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      const updatedDoc = {
-        $set: {
-          name: updateUser.name,
-          email: updateUser.email,
-        },
-      };
-      const result = await userCollection.updateOne(
-        filter,
-        updatedDoc,
-        options
-      );
-      res.send(result);
-    });
+//     // PUT User: update a existing users data
+//     app.put("/user/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const updateUser = req.body;
+//       const filter = { _id: ObjectId(id) };
+//       const options = { upsert: true };
+//       const updatedDoc = {
+//         $set: {
+//           name: updateUser.name,
+//           email: updateUser.email,
+//         },
+//       };
+//       const result = await userCollection.updateOne(
+//         filter,
+//         updatedDoc,
+//         options
+//       );
+//       res.send(result);
+//     });
 
-    // DELETE users from the database
-    app.delete("/user/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    });
-  } finally {
-    // await client.close();
-  }
-}
+//     // DELETE users from the database
+//     app.delete("/user/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const query = { _id: ObjectId(id) };
+//       const result = await userCollection.deleteOne(query);
+//       res.send(result);
+//     });
+//   } finally {
+//     // await client.close();
+//   }
+// }
 
-run().catch(console.dir);
+// run().catch(console.dir);
 
 //get
 app.get("/", (req, res) => {
