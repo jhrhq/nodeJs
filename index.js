ef3009b5508a58db2c7e5b668274e6720b58f190
jhrhq <jhr.code@proton.me>
2026-08-22 02:43:17 +0600

    tailwindcss added and public folder added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import path from "path";
+import "dotenv/config";
+
 const app = express();
-require("dotenv").config();
-const cors = require("cors");
 const port = process.env.PORT || 5000;
 // const { MongoClient, ServerApiVersion } = require("mongodb");
 // const ObjectId = require("mongodb").ObjectId;
@@ -9,84 +11,38 @@ const port = process.env.PORT || 5000;
 //user middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
+/*
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+*/
 
-// async function run() {
-//   try {
-//     await client.connect();
-//     const userCollection = client.db("foodExpress").collection("user");
-
-//     // get users as api in the front end
-//     app.get("/user", async (req, res) => {
-//       const query = {};
-
-//       const cursor = userCollection.find(query);
-//       const users = await cursor.toArray();
-//       res.send(users);
-//     });
-
-//     // get method: update a user
-
-//     app.get("/user/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: ObjectId(id) };
-//       const result = await userCollection.findOne(query);
-//       res.send(result);
-//     });
-
-//     // POST User : add a new user in the server
-//     app.post("/user", async (req, res) => {
-//       const newUser = req.body;
-//       console.log("adding newuser", newUser);
-//       const result = await userCollection.insertOne(newUser);
-//       res.send(result);
-//     });
-
-//     // PUT User: update a existing users data
-//     app.put("/user/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const updateUser = req.body;
-//       const filter = { _id: ObjectId(id) };
-//       const options = { upsert: true };
-//       const updatedDoc = {
-//         $set: {
-//           name: updateUser.name,
-//           email: updateUser.email,
-//         },
-//       };
-//       const result = await userCollection.updateOne(
-//         filter,
-//         updatedDoc,
-//         options
-//       );
-//       res.send(result);
-//     });
-
-//     // DELETE users from the database
-//     app.delete("/user/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: ObjectId(id) };
-//       const result = await userCollection.deleteOne(query);
-//       res.send(result);
-//     });
-//   } finally {
-//     // await client.close();
-//   }
-// }
-
+/*  async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("foodExpress").collection("user");
+  } finally {
+    // await client.close();
+  }
+}
+*/
 // run().catch(console.dir);
 
 //get
+
+const filePath = path.resolve();
+console.log("filepath ", filePath);
+
 app.get("/", (req, res) => {
-  res.send("running my crud server");
+  res.sendFile(filePath + "/public/views/index.html");
 });
 
 app.listen(port, () => {
-  console.log("listening" + port);
+  filePath;
+  console.log("listening " + port);
 });
