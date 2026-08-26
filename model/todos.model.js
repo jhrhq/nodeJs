16bdcfe90eb69c9e7d825d346753a2b0b992a8ab
jhrhq <jhr.code@proton.me>
2026-08-26 12:20:26 +0600

    schema virtual getter test

--- model/todos.model.js
+++ model/todos.model.js
@@ -10,9 +10,27 @@ const todoSchema = new mongoose.Schema(
       default: false,
       type: Boolean,
     },
-    title: { required: [true, 'Title is required'], trim: true, type: String },
+    title: {
+      required: [true, 'Title is required'],
+      trim: true,
+      type: String,
+      validate: {
+        message: (props) => `${props.value} is not a valid username (cannot contain 'admin')!`,
+        validator: (value) => !value.toLowerCase().includes('admin'),
+      },
+    },
   },
-  { timestamps: true },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } },
 );
+
+todoSchema.virtual('titleDescription').get(function () {
+  return `${this.title} ${this.description}`;
+});
+
+/* todoSchema.virtual('description').set(function (des) {
+  const parts = des.split(' ');
+  this.title = parts[0];
+  this.description = parts[1];
+}); */
 
 export default mongoose.model('Todo', todoSchema);
